e38329efdb046f4c90feb5d06355c3d9db31dc36
Alexandre Salvador <62520840+alexandresalvador@users.noreply.github.com>
2022-05-12 22:20:24 -0300

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div
+      v-for="(item, index) in user"
+      :key="index"
+      class="card"
+      style="width: 18rem"
+    >
+      <img :src="item.picture.medium" class="card-img-top" />
+      <div class="card-body">
+        <h3 class="card-title">{{ item.name.first }} {{ item.name.last }}</h3>
+        <h3 class="card-title">{{ item.email }}</h3>
+        <h3 class="card-title">Idade: {{ item.dob.age }}</h3>
+
+        <button @click="mudar" class="btn btn-primary">
+         Mudar
+        </button>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Card",
+  props: {
+    user: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    mudar() {
+      this.$emit("onChangeUser");
+    },
+  },
+};
+
+</script>
---
+++ src/views/users/User.vue
@@ -0,0 +1,45 @@
+<template>
+  <Card 
+  @onChangeUser="getUser" 
+  :user="user" />
+
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+import axios from "axios";
+
+export default {
+  name: "User",
+  data() {
+    return {
+      user: [],
+    };
+  },
+  components: {
+    Card,
+  },
+
+
+mounted() {
+    this.getUser();
+  },
+
+methods: {
+    getUser() {
+
+      const url = "https://randomuser.me/api/?results=1";
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data.results);
+          this.user = response.data.results;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
